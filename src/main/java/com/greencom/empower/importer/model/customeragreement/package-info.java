b47ae81023c1eaa24d4d17c82858461c02548320
Loïc Miguet <loic.miguet@avisto.com>
2019-04-11 11:52:37 +0200

    Removed JAXB classes and made them be generated instead.

--- src/main/java/com/greencom/empower/importer/model/customeragreement/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://iec.ch/TC57/2009/CustomerAgreement#", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.greencom.empower.importer.model.customeragreement;
