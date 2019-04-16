0d42efb0604107f61175b4b79befeab8cff49ee6
Loïc Miguet <loic.miguet@avisto.com>
2019-04-16 11:19:31 +0200

    Reworked the REST template configuration to support HTTPS proxy defined as environment variable 'https_proxy'.

--- src/main/java/com/greencom/empower/importer/config/RestTemplateConfig.java
+++ src/main/java/com/greencom/empower/importer/config/RestTemplateConfig.java
@@ -1,18 +1,16 @@
 package com.greencom.empower.importer.config;
 
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URI;
+import java.util.Arrays;
 
 @Configuration
 public class RestTemplateConfig {
@@ -20,22 +18,25 @@ public class RestTemplateConfig {
     @Value("${application.api.key}")
     private String apiKey;
 
+    @Value("#{systemEnvironment['https_proxy']}")
+    private String httpsProxy;
+
+
     @Bean
-    public RestTemplate getRestTemplate() {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
+    public RestTemplate restTemplate() {
 
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setHttpClient(httpClient);
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        if (StringUtils.isNotEmpty(this.httpsProxy)) {
+            URI proxyUri = URI.create(this.httpsProxy.contains("://") ? this.httpsProxy : ("http://" + this.httpsProxy));
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyUri.getHost(), proxyUri.getPort()));
+            requestFactory.setProxy(proxy);
+        }
+
         RestTemplate restTemplate = new RestTemplate(requestFactory);
-
-        restTemplate.getInterceptors().add(new ClientHttpRequestInterceptor() {
-            @Override
-            public ClientHttpResponse intercept(HttpRequest httpRequest, byte[] bytes, ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
-                httpRequest.getHeaders().set("GCN-APIKEY", apiKey);
-                return clientHttpRequestExecution.execute(httpRequest, bytes);
-            }
-        });
+        restTemplate.setInterceptors(Arrays.asList((r, b, e) -> {
+            r.getHeaders().set("GCN-APIKEY", this.apiKey);
+            return e.execute(r, b);
+        }));
         return restTemplate;
     }
-
 }
