f6e9acfdadbf5e52746d420ce4c5ad874b16864a
Michaël Gianfaldone <michael.gianfaldone@avisto.com>
2019-04-18 15:56:27 +0200

    Reformats and supplements Cron logging

--- src/main/java/com/greencom/empower/importer/scheduler/CustomerAgreementTask.java
+++ src/main/java/com/greencom/empower/importer/scheduler/CustomerAgreementTask.java
@@ -3,10 +3,12 @@ package com.greencom.empower.importer.scheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -38,7 +40,7 @@ public class CustomerAgreementTask {
     @Scheduled(cron = "${application.task.customeragreement.cron}")
     public void checkFiles() {
 
-        LOGGER.info("Start cron to check customer agreement files");
+        LOGGER.info("Cron: Start processing customer agreement files");
         try {
             List<Path> fileList = Files
                     .list(Paths.get(sourceFolder))
@@ -46,9 +48,13 @@ public class CustomerAgreementTask {
                     .filter(f -> f.toString().endsWith(".xml"))
                     .collect(Collectors.toList());
 
+            if ( fileList.isEmpty() ) {
+                LOGGER.info("Cron: no customer agreement files found");
+            }
+
             for (Path path : fileList) {
                 try {
-                    LOGGER.info("Found file {}", path.toString());
+                    LOGGER.info("Cron: found file {}", path.toString());
                     Path toProcessDirectoryPath = Paths.get(path.getParent().toString(), "to_process");
                     Path newPath = Paths.get(toProcessDirectoryPath.toString(), path.getFileName().toString());
                     if (!Files.exists(toProcessDirectoryPath)) {
@@ -56,27 +62,34 @@ public class CustomerAgreementTask {
                     }
 
                     Files.move(path, newPath, StandardCopyOption.REPLACE_EXISTING);
+                    LOGGER.info("Cron: moved file {} to {}", path,newPath);
 
-                    // The execution time is used to distinguish each instance of job execution
-                    Instant executionTime = Instant.now();
-                    jobLauncher.run(
-                            providerImporterJob,
-                            new JobParametersBuilder()
-                                    .addString("file", newPath.toString())
-                                    .addString("execution_time", executionTime.toString(), true)
-                                    .toJobParameters()
-                            );
+                    launchJob(newPath.toString());
+
                 } catch (IOException e) {
-                    LOGGER.error("Failed to move file {}", path);
+                    LOGGER.error("Cron: failed to move file {}", path);
                 } catch (Exception e) {
                     LOGGER.error(e.getMessage());
                 }
             }
         } catch (IOException e) {
-            LOGGER.error("Failed to list files in {}", sourceFolder);
+            LOGGER.error("Cron: failed to list files in {}", sourceFolder);
 
         } finally {
-            LOGGER.info("End of cron to check customer agreement files");
+            LOGGER.info("Cron: end of cron to check customer agreement files");
         }
+    }
+
+    private void launchJob(String filePath) throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+        LOGGER.info("Cron: launch {} batch job", providerImporterJob.getName());
+        // The execution time is used to distinguish each instance of job execution
+        Instant executionTime = Instant.now();
+        jobLauncher.run(
+                providerImporterJob,
+                new JobParametersBuilder()
+                        .addString("file", filePath)
+                        .addString("execution_time", executionTime.toString(), true)
+                        .toJobParameters()
+        );
     }
 }
