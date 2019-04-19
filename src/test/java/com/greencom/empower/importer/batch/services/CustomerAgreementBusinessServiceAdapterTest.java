184f2fae3b4e14401ea445f0856ac4ba6a0b9e40
Michaël Gianfaldone <michael.gianfaldone@avisto.com>
2019-04-19 16:45:35 +0200

    Add test suite for batch CA service and CA item writer

---
+++ src/test/java/com/greencom/empower/importer/batch/services/CustomerAgreementBusinessServiceAdapterTest.java
@@ -0,0 +1,47 @@
+package com.greencom.empower.importer.batch.services;
+
+import com.greencom.empower.importer.batch.exceptions.BatchFatalException;
+import com.greencom.empower.importer.batch.exceptions.BatchRecoverableException;
+import com.greencom.empower.importer.model.customeragreement.CustomerAgreement;
+import com.greencom.empower.importer.model.exception.CustomerAgreementException;
+import com.greencom.empower.importer.service.CustomerAgreementService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestClientException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerAgreementBusinessServiceAdapterTest {
+
+    @Mock
+    private CustomerAgreementService customerAgreementService;
+
+    @InjectMocks
+    private CustomerAgreementBusinessServiceAdapter customerAgreementBusinessServiceAdapter;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test(expected = BatchFatalException.class)
+    public void should_translate_exceptions_into_batch_fatal_exception() throws BatchFatalException, BatchRecoverableException {
+        doThrow(CustomerAgreementException.class).when(customerAgreementService).process((any(CustomerAgreement.class)));
+        customerAgreementBusinessServiceAdapter.processItem(new CustomerAgreement());
+    }
+
+    @Test(expected = BatchRecoverableException.class)
+    public void should_translate_exceptions_into_batch_recoverable_exception() throws BatchFatalException, BatchRecoverableException {
+        doThrow(RestClientException.class).when(customerAgreementService).process((any(CustomerAgreement.class)));
+        customerAgreementBusinessServiceAdapter.processItem(new CustomerAgreement());
+    }
+}
